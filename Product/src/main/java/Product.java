3010f8cf3b43080c534e943ee0f02710295fe10a
Yjx <1434402606@qq.com>
2019-12-07 15:04:48 +0800

    Prodct

---
+++ Product/src/main/java/Product.java
@@ -0,0 +1,12 @@
+import java.io.Serializable;
+
+/**
+ * @author 闫佳兴
+ * @data 2019/12/7 15:02
+ * @desc
+ */
+public class Product implements Serializable {
+    public static final long SERIALIZED = 1L;
+    private String name;
+    private String product;
+}
